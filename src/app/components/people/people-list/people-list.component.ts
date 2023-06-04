cc4f6b511c7b9890ef12583fd8eb70baded3e34e
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2023-06-04 20:24:35 +1200

    Add People List

--- src/app/components/people/people-list/people-list.component.ts
+++ src/app/components/people/people-list/people-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/person.model'
 
 @Component({
   selector: 'app-people-list',
   templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.css']
+  styleUrls: ['./people-list.component.css'],
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit {
+  people: Person[] = [
+    { id : 1, firstName : "Sandeep", lastName : "Perera"},
+    { id :2, firstName : "First", lastName :"Last"}
+  ];
 
+  constructor() {}
+  ngOnInit(): void {
+    this.people.push()
+  }
 }

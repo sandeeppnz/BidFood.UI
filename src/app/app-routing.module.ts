81ad4d152979bf97edd437a0818bee715c91d76e
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2023-06-04 19:15:48 +1200

    Add People list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PeopleListComponent
+},
+{
+  path:'people',
+  component: PeopleListComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent
   ],
   imports: [
     BrowserModule,
